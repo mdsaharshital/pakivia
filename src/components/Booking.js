4a2e1bd8248be79f7290213afdf105628483b34c
mdsaharshital <mdsaharshitol@gmail.com>
2022-11-09 00:26:10 +0600

    stuck on chnage of localstorage and cant rerender

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -3,23 +3,22 @@ import { useState } from "react";
 
 const Booking = () => {
   const [parkInfo, setParkInfo] = useState([]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const driversName = e.target.driversName.value;
     const vehicleNumber = e.target.vehicleNumber.value;
     const checkInTime = e.target.checkInTime.value;
     const parkInput = { driversName, vehicleNumber, checkInTime };
-    console.log("submit", parkInput);
-    console.log("uses", parkInfo);
     const hello = localStorage.getItem("items");
     let obj = JSON.parse(hello);
-    console.log(obj);
     setParkInfo([parkInput, ...parkInfo]);
     if (obj === null) {
       localStorage.setItem("items", JSON.stringify([parkInput, ...parkInfo]));
     } else {
       localStorage.setItem("items", JSON.stringify([...obj, parkInput]));
     }
+    e.target.reset();
   };
 
   return (
@@ -30,18 +29,21 @@ const Booking = () => {
           placeholder="Drivers name"
           name="driversName"
           className="input input-bordered m-3 w-64"
+          required
         />
         <input
           type="text"
           placeholder="Vehicle number"
           name="vehicleNumber"
           className="input input-bordered m-3 w-64"
+          required
         />
         <input
           type="datetime-local"
           placeholder="Type here"
           name="checkInTime"
           className="input input-bordered m-3"
+          required
         />
         <input
           type="submit"
--- src/components/ParkingList.js
+++ src/components/ParkingList.js
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 const ParkingList = () => {
+  const [parks, setParks] = useState([]);
   const hello = localStorage.getItem("items");
   let obj = JSON.parse(hello);
-  console.log(obj);
+  //
+  useEffect(() => {
+    setParks(obj);
+  }, []);
+  //
   return (
     <div className="p-3">
-      <h1>{obj === null ? "0 entries" : obj?.length + "Entries"}</h1>
-      {obj?.map((parkInfo, index) => (
-        <div key={index}>
-          <h1>Driver's name: {parkInfo?.driversName}</h1>
-          <h1>Vehicle number: {parkInfo?.vehicleNumber}</h1>
-        </div>
-      ))}
+      <h1>{parks === null ? "No car parked" : parks?.length + "Entries"}</h1>
+      <div className="overflow-x-auto">
+        {parks === null ? (
+          <> </>
+        ) : (
+          <table className="table table-zebra w-full">
+            {/* <!-- head --> */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Driver's name</th>
+                <th>Vehicle number</th>
+                <th>CheckInTime</th>
+              </tr>
+            </thead>
+            <tbody>
+              {parks?.map((parkInfo, index) => (
+                <tr key={index}>
+                  <th>{index + 1}</th>
+                  <td>{parkInfo?.driversName}</td>
+                  <td>{parkInfo?.vehicleNumber}</td>
+                  <td>{parkInfo?.checkInTime}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
     </div>
   );
 };
